3786b528670e0e8cfe8af5454916ff4dcf194745
itouzzane <itouzzane@gmail.com>
2022-09-13 10:31:39 +0100

    added new challenges

---
+++ Challenges C - Part 3/Challenge C9.c
@@ -0,0 +1,42 @@
+#include<stdio.h.>
+#include<stdlib.h>
+
+
+
+int main(){
+	
+int n= 0 , p = 0 , res = 1;
+
+do{
+	printf("entrez un nombre superieure strictement a 0  svp:");
+	scanf("%d",&n);
+}while(n<=0);
+
+do{
+	printf("\nentrez une puissance positif svp!!!!!!:");
+	scanf("%d",&p);
+}while(p<0);
+int d = p;
+
+while(n>0 && p>=0){
+	if(p==0){
+		res =  res * 1;
+		p--;
+		
+		
+}else{
+	    res = res * n ;
+		p--;
+	}
+	
+}
+printf(" %d a la puissance %d egal a : %d", n , d , res);
+
+
+
+
+
+	
+	
+	return 0;
+}
