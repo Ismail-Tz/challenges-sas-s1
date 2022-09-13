130858b8868d3cf3865ed5f992009e80d0c90cc0
itouzzane <itouzzane@gmail.com>
2022-09-13 13:56:58 +0100

    added challenge 4

--- Challenges C - Part 3/Challenge C4.c
+++ Challenges C - Part 3/Challenge C4.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <math.h>
 
-int main(){
+int main ()
+{
+int somme=0 , max = 0 , n =1;
+	
+while (n>0 && n<100){
+	 printf ("entrez un nombre :");
+     scanf("%d",&n);
+     
+     if (n==0){break;}
+     if (n>100){continue;}
+     
+     somme = somme + n;
+     if(max<n){
+     	max = n;
+	 }
+}
+	
+	
 
-    char digits[15], invDigits[15],i,j;
+	
+//  for(int i=1;i<10;i++){
+//  	printf ("entrez un nombre :");
+//	scanf("%d",&n);
+//	printf("\n");
+//  	if(n<100){
+//  		somme = somme + n;
+//  		if(max < n){
+//  	max = n;
+//  }
+//  }else if(n==0){
+//  	   break;
+//  } 
+//  }
+  
+  printf(" la somme de ces nombres est : %d \n", somme);
+  printf("le max de ces nombres est : %d", max);
+  
 
-    printf("Enter a number:");
-    scanf("%s", &digits);
-    int len = strlen(digits);
-    for (i=len ; i>0 ; i--){
-        strcat(invDigits[j],digits[i]);
-        j++;
-    };
-
-    printf("%s", invDigits);
-
-    return 0;
+return 0;
 }
